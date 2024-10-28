d5680cd31a37f45315c2ddd8dbe7175ffdae5160
Nabil Ben khemis <beardydjib@gmail.com>
2024-10-28 14:16:38 +0100

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,28 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(new FileWriter("result.out"));
+            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                writer.println(entry.getKey() + " " + entry.getValue());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+
+        }
+    }
+}
